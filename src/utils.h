06efe33dd6d25e14ccee74a949436d849a5a070d
FAIZ ASRA <faizasra30@gmail.com>
2026-06-13 11:56:48 +0700

    menambahkan dan merapikan komentar

--- src/utils.h
+++ src/utils.h
@@ -5,23 +5,20 @@
 #include <stdio.h>
 #include <time.h>
 
-// Struktur data untuk menyimpan salinan hasil pencarian Top-K di memori
-
-typedef struct CachedResult {
+typedef struct CachedResult
+{
   char **words;     // array kata
   long long *freqs; // array frekuensi kata
   int count;        // jumlah elemen yang valid dalam cache
   int k;            // nilai k yang digunakan saat pencarian
   double elapsed;   // waktu yang dibutuhkan untuk heapsort (ms)
-  int isValid; // penanda apakah cache berisi data yang valid (1) atau tidak (0)
+  int isValid;      // penanda apakah cache berisi data yang valid (1) atau tidak (0)
 } CachedResult;
 
 void sortDuration(clock_t start, clock_t end);
 void extractCollectionName(const char *path, char *outName, size_t maxLen);
-int generateOutputFile(CachedResult *freqRes, CachedResult *rareRes,
-                       const char *collection);
-void normalizePaths(const char *inputPath, char *docwordPath, char *vocabPath,
-                    size_t maxLen);
+int generateOutputFile(CachedResult *freqRes, CachedResult *rareRes, const char *collection);
+void normalizePaths(const char *inputPath, char *docwordPath, char *vocabPath, size_t maxLen);
 void freeCache(CachedResult *cache);
 
 #endif
