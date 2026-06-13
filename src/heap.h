59ddbcd78dcd36216de417c74cfb45f4cc6cb726
FAIZ ASRA <faizasra30@gmail.com>
2026-06-13 11:54:19 +0700

    menambahkan komentar struct

--- src/heap.h
+++ src/heap.h
@@ -1,31 +1,39 @@
 #ifndef HEAP_H
 #define HEAP_H
 
+// Alias
 typedef struct HeapNode *HeapNodePtr;
-typedef struct HeapNode {
+
+// Struktur data untuk menampung satu node/elemen kosakata
+typedef struct HeapNode
+{
   long long freq;
   int wordID;
 } HeapNode;
 
-typedef struct {
+// Struktur data untuk merepresentasikan Min-Heap
+typedef struct
+{
   HeapNodePtr data;
   int size;
   int capacity;
 } MinHeap;
 
-typedef struct {
+// Struktur data untuk merepresentasikan Max-Heap
+typedef struct
+{
   HeapNodePtr data;
   int size;
   int capacity;
 } MaxHeap;
 
-// MinHeap operations
+// Operasi Minheap
 void minHeapInit(MinHeap *heap, HeapNodePtr data_buffer, int capacity);
 void minHeapInsert(MinHeap *heap, HeapNode node);
 void minHeapifyDown(MinHeap *heap, int idx);
 void minHeapSort(MinHeap *heap);
 
-// MaxHeap operations
+// Operasi Maxheap
 void maxHeapInit(MaxHeap *heap, HeapNodePtr data_buffer, int capacity);
 void maxHeapInsert(MaxHeap *heap, HeapNode node);
 void maxHeapifyDown(MaxHeap *heap, int idx);
